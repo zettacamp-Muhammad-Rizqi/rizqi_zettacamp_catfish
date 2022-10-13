f04151d43c12c64874c7a7d02f5808d0c8b2bc2a
zettacamp-Muhammad-Rizqi <muhammad.rizqi@zettacamp.pro>
2022-10-13 09:38:27 +0700

    Fix the code to with no multiple function

--- js_day2.js
+++ js_day2.js
@@ -7,69 +7,52 @@ const book = {
     discount : 30,
     stock : 3,
     purchased : 2,
-    isCredit : true,
+    isCredit : false,
     creditMonth : 6,
     credit : [],
 }
 
-//Destructuring object
-let {creditMonth} = book
+buyBook(book) //Call the function
 
-console.log(creditMonth)
-console.log(book)
-
-let discountPrice = book.price*(book.discount/100)
-let taxPrice = book.price*(book.tax/100)
-let bookPriceDiscount= book.price - discountPrice
-let bookPriceTax = bookPriceDiscount + taxPrice
-
-//Price a month for credit
-// let payForMonth = bookPriceTax/creditMonth
-
-//Condition for use credit or not
-if(book.isCredit==true){
-    buyWithCredit(book)
-}
-else{
-    buyBook(book)
-}
-
-//Normal without credit balance
 function buyBook(buku){
-    let totalPay = 0;
-    for(let i=1; i<=buku.purchased; i++){
-        if(buku.available){
-            totalPay +=bookPriceTax;
-            buku.stock -=1
-            if(buku.stock<=0){
-                buku.available = false
-                break;
+    let {creditMonth} = book //Destructuring object to make creditMonth from the book too make local variabel
+    let discountPrice = book.price*(book.discount/100) //count the discount price
+    let taxPrice = book.price*(book.tax/100) // count the tax price
+    let bookPriceDiscount= book.price - discountPrice //count the book after get discount
+    let bookPriceTax = bookPriceDiscount + taxPrice // count the book after get discount and get tax
+
+    if(book.isCredit==true){
+        let payForMonth = Math.round(bookPriceTax/creditMonth)
+        let payCredit=0
+        for(let i=1; i<=buku.creditMonth; i+=1){
+            payCredit+=payForMonth
+            buku.credit.push({
+                month : i,
+                payMonth : payForMonth,
+                totalPayMonth : payCredit,
+            })
+        }
+        console.log(...buku.credit)
+    }
+    else{
+        let totalPay = 0;
+        for(let i=1; i<=buku.purchased; i++){
+            if(buku.available){
+                totalPay +=bookPriceTax;
+                buku.stock -=1
+                if(buku.stock<=0){
+                    buku.available = false
+                    break;
+                }
             }
         }
+
+        console.log("Amount of Discount = "+discountPrice)
+        console.log("Price after Discount = "+bookPriceDiscount)
+        console.log("Amount of Tax = "+taxPrice)
+        console.log("Price after tax = "+bookPriceTax)
+        console.log("Total you must pay is "+totalPay)
+        book.stock > 0 ? console.log("Amount of book after purchasing can be purchased again") : console.log("Amount of book after purchasing can't be purchased again")
     }
-
-    console.log("Amount of Discount = "+discountPrice)
-    console.log("Price after Discount = "+bookPriceDiscount)
-    console.log("Amount of Tax = "+taxPrice)
-    console.log("Price after tax = "+bookPriceTax)
-    console.log("Total you must pay is "+totalPay)
-    book.stock > 0 ? console.log("Amount of book after purchasing can be purchased again") : console.log("Amount of book after purchasing can't be purchased again")
+    
 }
-
-//function push data into credit
-function buyWithCredit(buku){
-    let payForMonth = Math.round(bookPriceTax/creditMonth)
-    let payCredit=0
-    for(let i=1; i<=buku.creditMonth; i+=1){
-        payCredit+=payForMonth
-        // buku.credit.push(payCredit)
-        buku.credit.push({
-            month : i,
-            payMonth : payForMonth,
-            totalPayMonth : payCredit,
-        })
-    }
-    console.log(...buku.credit)
-}
-
-//Ingat untuk Revisi ke 1 Fungsi saja!!!
