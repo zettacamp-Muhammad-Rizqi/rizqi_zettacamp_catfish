afa9e0a3119f2847a6bfbd5635d60f7eeec84900
zettacamp-Muhammad-Rizqi <muhammad.rizqi@zettacamp.pro>
2022-10-14 08:59:17 +0700

    add the looping and statement on function duration

--- js_day4.js
+++ js_day4.js
@@ -1,63 +1,134 @@
 const songs = [
     {
+        no : 1,
         title : "The Color Violet",
         artist : "Tory Lanez",
         duration : 4,
         genre : "pop",
     },
     {
+        no : 2,
         title : "You Belong With Me",
         artist : "Taylor Swift",
         duration : 3,
         genre : "pop",
     },
     {
+        no : 3,
         title : "The Shadow",
         artist : "Blues Cousins",
         duration : 7,
         genre : "jazz",
     },
     {
+        no : 4,
         title : "Begadang",
         artist : "Rhoma Irama",
         duration : 3,
         genre : "dangdut",
     },
     {
+        no : 5,
         title : "Faded",
         artist : "Alan Walker",
         duration : 3,
         genre : "edm",
     },
     {
+        no : 6,
         title : "FadedSing Me to Sleep",
         artist : "Alan Walker",
         duration : 3,
         genre : "edm",
     },
     {
+        no : 7,
         title : "Judi",
         artist : "Rhoma Irama",
         duration : 5,
         genre : "dangdut",
     },
+    {
+        no : 8,
+        title : "Tak Pernah Ternilai",
+        artist : "Last Child",
+        duration : 5,
+        genre : "pop",
+    },
+    {
+        no : 9,
+        title : "Rider",
+        artist : "Saykoji",
+        duration : 3,
+        genre : "rap",
+    },
+    {
+        no : 10,
+        title : "Not Afraid",
+        artist : "Eminem",
+        duration : 4,
+        genre : "rap",
+    },
+    {
+        no : 11,
+        title : "Venom",
+        artist : "Eminem",
+        duration : 5,
+        genre : "rap",
+    },
+    {
+        no : 12,
+        title : "What a wonderful World",
+        artist : "Louis Armstrong",
+        duration : 2,
+        genre : "jazz",
+    },
+    {
+        no : 13,
+        title : "Craving You",
+        artist : "Thomas Rhett",
+        duration : 5,
+        genre : "country",
+    },
+    {
+        no : 14,
+        title : "Wagon Wheel",
+        artist : "Darius Rucker",
+        duration : 5,
+        genre : "country",
+    },
+    {
+        no : 15,
+        title : "Yours If You Want It",
+        artist : "Rascal Flatts",
+        duration : 5,
+        genre : "country",
+    },
 ]
 
-groupByArtist(songs)
-groupByGenre(songs)
+// groupByArtist(songs, "Alan Walker")
+// groupByGenre(songs, "rap")
+groupByDuration(songs, 60)
 
-function groupByArtist(songs){
-    let artistName = songs.filter(artistName=>artistName.artist==="Alan Walker")
+function groupByArtist(songs, name){
+    let artistName = songs.filter(artistName=>artistName.artist===name)
     console.log(...artistName)
 }
 
-function groupByGenre(songs){
-    let genreName = songs.filter(genreName=>genreName.genre==="dangdut")
-    console.log(...genreName)
+function groupByGenre(songs, nameGenre){
+    let genreName = songs.filter(genreName=>genreName.genre===nameGenre)
+    // console.log(...genreName)
 }
 
-function groupByDuration(){
+function groupByDuration(songs, minute){
+    console.log("grupbyDuration")
     //this function for group songs less one hour
-
-    //durationTotal = (this optional if you can make it)
+    
+    let addition = 0
+    for(let i=0; i<songs.length; i++){
+        addition +=songs[i].duration
+        if(addition<minute){
+            console.log(songs[i].duration)
+        }
+    }
 }
