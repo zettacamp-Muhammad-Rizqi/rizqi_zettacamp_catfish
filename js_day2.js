68c6da20ef9546219e0e97ee5d21365c4b1d34d0
zettacamp-Muhammad-Rizqi <muhammad.rizqi@zettacamp.pro>
2022-10-24 11:09:01 +0700

    initial commit

--- js_day2.js
+++ js_day2.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose')
 
 //Make object
 const book = {
@@ -10,63 +11,12 @@ const book = {
     price : 100000,
     tax : 12,
     discount : 30,
-    // stock : 3,
-    // purchased : 2,
-    // creditMonth : 6,
-    // credit : [],
-    // bill : [],
 }
 
 //root page
 app.get('/', authentication, (req, res)=>{
     res.send(book)
 })
-
-//Events
-// const Events = require('events')
-// const myEvents = new Events()
-
-// myEvents.on('program_start', (message)=>{
-//     console.log(`Program is beginning, ${message}`)
-// })
-// myEvents.emit('program_start', 'Rizqi') // fire or trigger
-
-// //GET 'buku.txt' route async with await
-// const fs = require('fs/promises')
-// app.get('/readbuku', authentication, async (req, res)=>{
-//     try{
-//         let data = await fs.readFile('./buku.txt', {encoding:'utf-8'})
-//         console.log(data)
-//         res.send(data)
-//     }catch(err){
-//         console.log("Get an Error!")
-//         res.send(err)
-//     }
-// })
-
-// //no await
-// app.get('/readbukuNoAwait', authentication, (req, res)=>{
-//     let data = fs.readFile('./buku.txt', {encoding:'utf-8'})
-//     data
-//         .then((result)=>{
-//             console.log(result)
-//             res.send(result)
-//         })    
-//         .catch((err)=>{
-//             console.log("Get an Error!")
-//             res.send(err)
-//         })
-// })
-
-
-//GET credit with async await
-// app.get('/buyCredit', authentication, async(req, res) => {
-    
-//     let newBook = buyBook(book)
-//     console.log(newBook)
-//     newBook = await calculateCredit(book)
-//     res.send(book)
-// })
 
 //Make new Map for new object
 const titleMap = new Map()
@@ -75,6 +25,7 @@ titleMap.set("Matematika", {...book, title:"Matematika"})
 titleMap.set("IPS", {...book, title:"IPS"})
 titleMap.set("IPA", {...book, title:"IPA"})
 // console.log(titleMap.get("IPS"))
+
 // new Set for new object
 const titleSet = new Set(["Ultraman", "Matematika", "IPS"])
 
@@ -128,68 +79,3 @@ function authentication(req, res, next){
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
-
-// //Calculate credit Month
-// async function calculateCredit(buku, additionMonth=600, _month=3){
-//     let {creditMonth} = book //Destructuring object to make creditMonth from the book too make local variabel
-//     let fixPrice = buku.bill[0].priceAftertax
-
-//     let payForMonth = Math.round(fixPrice/creditMonth)
-//     let payCredit=0
-//     let arrayCredit = []
-//     for(let i=1; i<=creditMonth; i+=1){
-//         payCredit+=payForMonth
-//         if(i===_month){
-//             payForMonth+=additionMonth
-//             arrayCredit.push({
-//                 month : i,
-//                 payMonth : payForMonth,
-//                 totalPayMonth : payCredit,
-//             })
-//         }else{
-//             arrayCredit.push({
-//                 month : i,
-//                 payMonth : payForMonth,
-//                 totalPayMonth : payCredit,
-//             })
-//         }
-//     }
-//     buku.credit = arrayCredit
-//     return buku
-
-// }
-
-// function buyBook(buku){
-    
-//     let discountPrice = buku.price*(buku.discount/100) //count the discount price
-//     let taxPrice = buku.price*(buku.tax/100) // count the tax price
-//     let bookPriceDiscount= buku.price - discountPrice //count the book after get discount
-//     let bookPriceTax = bookPriceDiscount + taxPrice // count the book after get discount and get tax
-
-//     let totalPay = 0;
-//     for(let i=1; i<=buku.purchased; i++){
-//         if(buku.available){
-//             totalPay +=bookPriceTax;
-//             buku.stock -=1
-//             if(buku.stock<=0){
-//                 buku.available = false
-//                 break;
-//             }
-//         }
-//     }
-
-//     buku.bill.push({
-//         amountOfDiscount : discountPrice,
-//         priceAfterDiscount : bookPriceDiscount,
-//         amountOfTax : taxPrice,
-//         priceAftertax : bookPriceTax,
-//         "Total you must pay is" : totalPay,
-//     })
-
-//     if (book.stock > 0 == true){
-//         buku.bill.push("Amount of book after purchasing can be purchased again")
-//     }else{
-//         buku.bill.push("Amount of book after purchasing can't be purchased again")
-//     }
-//     return buku
-// }
