f7d65bfcc25f0345252cfa6104c85e8a936aab63
zettacamp-Muhammad-Rizqi <muhammad.rizqi@zettacamp.pro>
2022-10-28 16:39:14 +0700

    Done make addFields for multiply

--- books.js
+++ books.js
@@ -13,13 +13,15 @@ async function connected(){
 connected()
 
 //Book Shema
-const bookSchema = new mongoose.Schema({
-    title: String,
-	author: String,
-	date_published: Date,
-	price: Number,
-  },
-  {timestamps : true}
+const bookSchema = new mongoose.Schema(
+    {
+        title: String,
+        author: String,
+        date_published: Date,
+        price: Number,
+        stock : Number
+    },
+    {timestamps : true}
   )
     
   const books = mongoose.model("books", bookSchema);
@@ -29,64 +31,62 @@ app.get('/', express.urlencoded({extended:true}), async (req, res) => {
     const showData = await books.find()
     res.send(showData)
 })
-
-//find command
-app.post('/find', express.urlencoded({extended:true}), async (req, res) => {
-    const {find} = req.body
-    const findData = await books.findOne(
-        {
-            title : find
-            // _id : find //remove the comment and make title comment if want to use
-        }
-    )
-    res.send(findData)
-})
   
 //insert the book to collection
 app.post('/insertBook', express.urlencoded({extended:true}), async (req, res) => {
-    const {title, author, birth, price} = req.body
+    const {title, author, price, stock} = req.body
 
-    const insert = await books.collection.insertOne(
+    const book = new books(
         {
-            title,
-            author,
-            birth,
-            price,
-            created_At : new Date()
+            title: title,
+            author: author,
+            date_published: new Date(),
+            price: price,
+            stock : stock
         }
     )
-
-    const book = await books.findById(insert.insertedId.toString())
-
-    res.send(book)
+    const saveBook = await book.save()
+    res.send(saveBook)
 })
 
-//update book command
-app.put('/update', express.urlencoded({extended:true}), async (req, res) => {
-    const {id, newTitle} = req.body
-    const updateData = await books.findByIdAndUpdate(
-        id,
+//aggregate addFields for multiply stock and price
+app.get("/addFields",async(req,res)=>{
+
+      const addField= await books.aggregate([
         {
-            updated_At : new Date(),
-            title : newTitle
-        }, {
-            new : true
+          $addFields:{
+            total_price:{
+              $multiply:["$stock","$price"]
+            }
+          }
         }
-    )
-    
-    res.send(updateData)
-})
+      ])
+      res.send(addField)
+});
 
-//delete command
-app.delete('/delete', express.urlencoded({extended:true}), async (req, res) => {
-    const {clear} = req.body
-    const deleteData = await books.deleteOne(
-        {
-            clear
-        }
-    )
-    res.send(deleteData)
-})
+//aggregate with project
+app.post("/project",async(req,res)=>{
+    const {find} = req.body
+    if(find){
+        const findTitle = await books.aggregate([
+            {
+                $match:{
+                    title: find
+                }
+            }
+        ])
+        res.send(findTitle)
+    }else{
+        const findTitle2 = await books.aggregate([
+            {
+                $project:{
+                    title:1, date_published:1
+                }
+            }
+        ])
+        res.send(findTitle2)
+    }
+});
 
 //Listen port
 app.listen(port, () => {
