79fee9b08fc011395c9617341a5da2dc883f8454
zettacamp-Muhammad-Rizqi <muhammad.rizqi@zettacamp.pro>
2022-11-08 14:17:18 +0700

    Add bcryptjs for encrypted password

--- src/Users/regist.users.js
+++ src/Users/regist.users.js
@@ -1,8 +1,19 @@
-const {users} = require('../model/Users')
+const { ApolloError } = require('apollo-server-errors')
+const {users} = require('../model/Users') //Import model users
+const bcrypt = require('bcryptjs')
+
 const registUser = async (_, {register:{password, email, last_name, first_name}})=>{
+    const oldUser = await users.findOne({email})
+
+    if(oldUser){
+        throw new ApolloError('A user is already registered with the email: '+email, ' User Already Exist')
+    }
+
+    let encryptedPassword = await bcrypt.hash(password, 10)
+
     const user = new users(
         {
-            password: password,
+            password: encryptedPassword,
             email: email,
             last_name: last_name,
             first_name: first_name,
