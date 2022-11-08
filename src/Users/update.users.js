0519a4f5960d7c2a84eecbe3e1e04e735991279c
zettacamp-Muhammad-Rizqi <muhammad.rizqi@zettacamp.pro>
2022-11-08 16:02:43 +0700

    Done make update user with unique email

--- src/Users/update.users.js
+++ src/Users/update.users.js
@@ -1,19 +1,52 @@
-//import model users
-const {users} = require('../model/Users')
+const { default: mongoose } = require('mongoose')
+const { ApolloError } = require('apollo-server-errors')
+const bcrypt = require('bcryptjs')
+const {users} = require('../model/Users') //import model users
 
-const updateUser = async (_, {updateUser:{email}})=>{
-    const update = await users.findByIdAndUpdate(
-        {
-            email: email
-        },
-        {
-            $set: {
-                status : "deleted"
+const updateUser = async (_, {update:{_id, password, email, last_name, first_name}})=>{
+    const oldUser = await users.findOne({email})
+
+    if(oldUser){
+        throw new ApolloError('A user is already with the email: '+email)
+    }
+    if(password){
+        let encryptedPassword = await bcrypt.hash(password, 10)
+        const update = await users.findByIdAndUpdate(
+            {
+                _id : mongoose.Types.ObjectId(_id)
+            },
+            {
+                $set: {
+                    password: encryptedPassword,
+                    email: email,
+                    last_name: last_name,
+                    first_name: first_name,
+                }
+            },
+            {
+                new: true
             }
-        }
-    )
-
-    return update
+        )
+        return update
+    }
+    if(!password){
+        const update = await users.findByIdAndUpdate(
+            {
+                _id : mongoose.Types.ObjectId(_id)
+            },
+            {
+                $set: {
+                    email: email,
+                    last_name: last_name,
+                    first_name: first_name,
+                }
+            },
+            {
+                new: true
+            }
+        )
+        return update
+    }
 }
 
 module.exports = {updateUser}
